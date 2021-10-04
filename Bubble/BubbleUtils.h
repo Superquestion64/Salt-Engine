7b0e355ba0296fe8c66877e56b8043922528cb1d
Superquestion64 <charles.vega56@myhunter.cuny.edu>
2021-10-04 15:33:31 -0400

    Initial commit 2

--- Bubble/BubbleUtils.h
+++ Bubble/BubbleUtils.h
@@ -4,7 +4,8 @@
 	#ifdef BUBBLE_LIB
 		#define BUBBLE_API __declspec(dllexport)
 	#else
-	#define BUBBLE_API __declspec(dllimport)
+		#define BUBBLE_API __declspec(dllimport)
 	#endif
 #else
+	#define BUBBLE_API
 #endif
